f3a748e38c91fec7d381f96355d12b4ceffb534c
LosEvons <emil.malk@gmail.com>
2023-04-27 13:07:13 +0300

    System specific SDL include added

---
+++ include/SDL2.h
@@ -0,0 +1,5 @@
+#ifdef _WIN32
+	#include <SDL2.h>
+#else
+	#include <SDL2/SDL.h>
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
-#include <SDL2/SDL.h>
+#include "SDL2.h"
+
 #include <iostream>
 
 int main(int argc, char *argv[]){
